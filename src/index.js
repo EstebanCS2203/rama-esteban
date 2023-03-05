b18bf1e4be374ddb7aab4a94309f352a305be20a
1053Gonzalez <jose.gonzalez.6000@miremington.edu.co>
2023-03-05 18:38:55 -0500

    se elimina colegios_1.jpg ya que se usa desde la web se agrega el componente Footer al index.js y se instancia en el index.html

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Footer from './componentes/footer/Footer';
+import './componentes/footer/Footer.css'
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <Footer />
   </React.StrictMode>
 );
 
