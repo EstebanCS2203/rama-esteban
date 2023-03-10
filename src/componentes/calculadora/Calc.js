34f04c0af4158dc5cfaaf914286c1696dbd0a431
1053Gonzalez <jose.gonzalez.6000@miremington.edu.co>
2023-03-10 15:24:41 -0500

    se modifica la etiqueta generica button a button-calc para que no interfiera con los demas botones

--- src/componentes/calculadora/Calc.js
+++ src/componentes/calculadora/Calc.js
@@ -26,21 +26,21 @@ function Calc() {
         <div className="calculator">
             <div id="screen">{screenValue}</div>
             <ul id="buttons">
-                <li><button >⚛</button></li>
-                <li><button onClick={handleButtonClick} data-key="clear">C</button></li>
-                <li><button onClick={handleButtonClick} data-key="/">/</button></li>
-                <li><button onClick={handleButtonClick} data-key="*">*</button></li>
-                <li><button onClick={handleButtonClick} data-key="7">7</button></li>
-                <li><button onClick={handleButtonClick} data-key="8">8</button></li>
-                <li><button onClick={handleButtonClick} data-key="9">9</button></li>
-                <li><button onClick={handleButtonClick} data-key="-">-</button></li>
-                <li><button onClick={handleButtonClick} data-key="4">4</button></li>
-                <li><button onClick={handleButtonClick} data-key="5">5</button></li>
-                <li><button onClick={handleButtonClick} data-key="6">6</button></li>
-                <li><button onClick={handleButtonClick} data-key="+">+</button></li>
-                <li><button onClick={handleButtonClick} data-key="1">1</button></li>
-                <li><button onClick={handleButtonClick} data-key="2">2</button></li>
-                <li><button onClick={handleButtonClick} data-key="3">3</button></li>
+                <li><button id="button-calc">⚛</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="clear">C</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="/">/</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="*">*</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="7">7</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="8">8</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="9">9</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="-">-</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="4">4</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="5">5</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="6">6</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="+">+</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="1">1</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="2">2</button></li>
+                <li><button id="button-calc" onClick={handleButtonClick} data-key="3">3</button></li>
                 <li><button onClick={handleButtonClick} data-key="equal" id="igual">=</button></li>
                 <li><button onClick={handleButtonClick} data-key="0" id="cero">0</button></li>
                 <li><button onClick={handleButtonClick} data-key="." id="punto">.</button></li>
