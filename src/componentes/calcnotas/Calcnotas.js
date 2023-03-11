2bce216d5b55676957e6db29a161376c0766db66
1053Gonzalez <jose.gonzalez.6000@miremington.edu.co>
2023-03-10 21:16:26 -0500

    se corrigen etiquetas que alteraban otros elementos

--- src/componentes/calcnotas/Calcnotas.js
+++ src/componentes/calcnotas/Calcnotas.js
@@ -7,24 +7,24 @@ function Calcnotas(){
         <div className="calculadora-nota">
 		<h2 id="titulocalc">Calculadora de Nota Definitiva</h2>
 		<div id="input-container">
-			<label for="q1">Nota 1:</label>
+			<label id="label-notas" for="q1">Nota 1:</label>
 			<input type="number" id="q1" min="0" max="5"></input>
 		</div>
 		<div id="input-container">
-			<label for="q2">Nota 2:</label>
+			<label id="label-notas" for="q2">Nota 2:</label>
 			<input type="number" id="q2" min="0" max="5"></input>
 		</div>
 		<div id="input-container">
-			<label for="q3">Nota 3:</label>
+			<label id="label-notas" for="q3">Nota 3:</label>
 			<input type="number" id="q3" min="0" max="5"></input>
 		</div>
 		<div id="input-container">
-			<label for="q4">Nota 4:</label>
+			<label id="label-notas" for="q4">Nota 4:</label>
 			<input type="number" id="q4" min="0" max="5"></input>
 		</div>
 		<button id="calculadoranotas" onclick="calculate()">Calcular Nota Definitiva</button>
 		<div id="output-container">
-			<label for="result">Nota Definitiva:</label>
+			<label id="label-notas" for="result">Nota Definitiva:</label>
 			<input type="number" id="result" disabled></input>
 			<div id="message"></div>
 		</div>
