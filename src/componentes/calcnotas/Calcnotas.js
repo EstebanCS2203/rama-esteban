d7cb2f36748cc636ee47710f30ea7b06f3566084
EstebanCS2203 <esteban.causado.8914@miremington.edu.co>
2023-03-10 20:05:23 -0500

    Se agrega la calculadora para notas con su respectiva estructura y diseño, aun falta logica

---
+++ src/componentes/calcnotas/Calcnotas.js
@@ -0,0 +1,35 @@
+
+import './Calcnotas.css'
+
+function Calcnotas(){
+	
+	return(
+        <div className="calculadora-nota">
+		<h2 id="titulocalc">Calculadora de Nota Definitiva</h2>
+		<div id="input-container">
+			<label for="q1">Nota 1:</label>
+			<input type="number" id="q1" min="0" max="5"></input>
+		</div>
+		<div id="input-container">
+			<label for="q2">Nota 2:</label>
+			<input type="number" id="q2" min="0" max="5"></input>
+		</div>
+		<div id="input-container">
+			<label for="q3">Nota 3:</label>
+			<input type="number" id="q3" min="0" max="5"></input>
+		</div>
+		<div id="input-container">
+			<label for="q4">Nota 4:</label>
+			<input type="number" id="q4" min="0" max="5"></input>
+		</div>
+		<button id="calculadoranotas" onclick="calculate()">Calcular Nota Definitiva</button>
+		<div id="output-container">
+			<label for="result">Nota Definitiva:</label>
+			<input type="number" id="result" disabled></input>
+			<div id="message"></div>
+		</div>
+        <button id="limpiar" onclick="limpiar()">Limpiar</button>
+	</div>
+    )
+}
+export default Calcnotas;
